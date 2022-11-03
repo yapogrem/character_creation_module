b696f804deecd1192d582925c061cce6474b89b1
Гриценко Евгений <ya0961@yandex.ru>
2022-11-04 01:41:24 +0300

    Added docstrings for functions

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from random import randint
 
+# Новый импорт.
+# Из модуля start_game_banner, который расположен в папке graphic_arts,
+# импортируем функцию run_screensaver().
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
